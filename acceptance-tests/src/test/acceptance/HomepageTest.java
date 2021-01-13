e6ff25433d9141424be7b85541fe8abe53c50de2
DogSobre <juanfernandoherrada13@gmail.com>
2021-01-13 14:36:07 +0100

    update

--- acceptance-tests/src/test/acceptance/HomepageTest.java
+++ acceptance-tests/src/test/acceptance/HomepageTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 	features = {"../src/test/acceptance"}, // ou se situe votre fichier .feature
